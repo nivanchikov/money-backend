24e1b466b2ef632e8a91b302f91b721d6a3f6808
Nikita Ivanchykov <nivanchikov.dev@gmail.com>
2022-02-09 22:42:29 +0200

    Update logging

--- Sources/App/LoggingMiddleware.swift
+++ Sources/App/LoggingMiddleware.swift
@@ -1,10 +1,12 @@
 import Vapor
 
 struct LoggingMiddleware: AsyncMiddleware {
+	let logger: Logger
+
 	func respond(to request: Request, chainingTo next: AsyncResponder) async throws -> Response {
 		let response = try await next.respond(to: request)
 
-		request.logger.warning("Request \(request)\nResponse \(response)", metadata: nil)
+		logger.warning("Request \(request)\nResponse \(response)", metadata: nil)
 
 		return response
 	}
